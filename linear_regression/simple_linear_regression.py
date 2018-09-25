c4244d310f85bc9281e8b10b3e3c442e34df5e39
steve <vuanhtu1993mta@gmail.com>
2018-09-25 08:52:17 +0700

    update

--- linear_regression/simple_linear_regression.py
+++ linear_regression/simple_linear_regression.py
@@ -1,46 +1,42 @@
-# Simple Linear Regression
-
-# Importing the libraries
-import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
+import numpy as np
 
-# Importing the dataset
-dataset = pd.read_csv('Salary_Data.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 1].values
+my_data = np.genfromtxt('data.csv', delimiter=',')  # read the data
+X = my_data[:, 0].reshape(-1, 1)  # -1 tells numpy to figure out the dimension by itself
+ones = np.ones([X.shape[0], 1])  # create a array containing only ones
+X = np.concatenate([ones, X], 1)  # cocatenate the ones to X matrix
+y = my_data[:, 1].reshape(-1, 1)  # create the y matrix
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
+# notice small alpha value
+alpha = 0.0001
+iters = 1000
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=1 / 3, random_state=0)
+# theta is a row vector
+theta = np.array([[1.0, 1.0]])
 
-# Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
 
-# Fitting Simple Linear Regression to the Training set
-from sklearn.linear_model import LinearRegression
+def computeCost(X, y, theta):
+    inner = np.power(((X @ theta.T) - y), 2)
+    # @ means matrix multiplication of arrays. If we want to use * for multiplication we will have to convert all arrays to matrices
+    return np.sum(inner) / (2 * len(X))
 
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
 
-# Predicting the Test set results
-y_pred = regressor.predict(X_test)
+def gradientDescent(X, y, theta, alpha, iters):
+    for i in range(iters):
+        theta = theta - (alpha / len(X)) * np.sum((X @ theta.T - y) * X, axis=0)
+        cost = computeCost(X, y, theta)
+        # if i % 10 == 0: # just look at cost every ten loops for debugging
+        #     print(cost)
+    return theta, cost
 
-# Visualising the Training set results
-plt.scatter(X_train, y_train, color='red')
-plt.plot(X_train, regressor.predict(X_train), color='blue')
 
-# Visualising the Test set results
-plt.scatter(X_test, y_test, color='red')
-plt.plot(X_train, regressor.predict(X_train), color='blue')
-plt.title('Salary vs Experience (Test set)')
-plt.xlabel('Years of Experience')
-plt.ylabel('Salary')
+g, cost = gradientDescent(X, y, theta, alpha, iters)
+
+plt.figure(1)
+plt.scatter(my_data[:, 0].reshape(-1, 1), y)
+axes = plt.gca()
+x_vals = range(0, len(X))
+print(x_vals)
+y_vals = g[0][0] + g[0][1] * x_vals  # the line equation
+plt.plot(x_vals, y_vals, '--')
 plt.show()
-
